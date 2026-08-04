b65624b8f75a61d397eadffb4d4296fe55110a95
Saimum Arafat <saimumarafat4@gmail.com>
2026-08-05 00:28:40 +0600

    Time: 0 ms (100%), Space: 9.9 MB (48.76%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.c
@@ -0,0 +1,20 @@
+char* intToRoman(int num) {
+    static char result[20];
+
+    int values[] = {1000, 900, 500, 400, 100, 90, 50, 40,
+                    10, 9, 5, 4, 1};
+
+    char *symbols[] = {"M", "CM", "D", "CD", "C", "XC", "L",
+                       "XL", "X", "IX", "V", "IV", "I"};
+
+    result[0] = '\0';
+
+    for (int i = 0; i < 13; i++) {
+        while (num >= values[i]) {
+            strcat(result, symbols[i]);
+            num -= values[i];
+        }
+    }
+
+    return result;
+}
